4a6020f7a5e8ea311a6d762a2785ec85449e573e
Egor <esmirnov1234@gmail.com>
2025-10-17 21:08:12 +0300

    fix type annotation

--- backend/fill_qdrant.py
+++ backend/fill_qdrant.py
@@ -1,15 +1,16 @@
-import pandas as pd
 import re
+
+import pandas as pd
+from qdrant_client import models
 
 from application.db.qdrant_repo import QdrantRepository
 from application.services.ml import embedding_model
-from qdrant_client import models
 
 df = pd.read_csv('data_cleaned.csv', sep=';')
 
 
 # "POINT (44.003277 56.331576)"
-def parse_coordinates(coord_str):
+def parse_coordinates(coord_str: str) -> dict:
     match = re.match(r'POINT\s*\(([\d.]+)\s+([\d.]+)\)', coord_str.strip())
     if match:
         lat = float(match.group(2))
@@ -52,7 +53,4 @@ for i, (payload, vector) in enumerate(zip(payloads, vectors)):
     )
     points_to_upsert.append(point)
 
-print('Загрузка данных в Qdrant...')
 repo.client.upsert(collection_name=repo.collection_name, points=points_to_upsert)
-
-print(f'Загрузка завершена! Всего загружено {len(points_to_upsert)} записей в Qdrant')
