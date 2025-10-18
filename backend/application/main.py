9fa3934f2b50253a07af2caaf4458233b0435df5
User <and12062005@yandex.ru>
2025-10-18 20:45:52 +0300

    add: returning llm answer to user

--- backend/application/main.py
+++ backend/application/main.py
@@ -7,7 +7,7 @@ import uvicorn
 
 import db.qdrant_repo
 import models.place_payload
-import schemas.user_input
+import schemas.user_io
 import services.ml
 
 MAX_PLACES_COUNT = 5
@@ -18,22 +18,28 @@ app = fastapi.FastAPI()
 
 @app.post('/handle')
 def handle_input(
-    data: schemas.user_input.UserInput,
-    )-> dict[models.place_payload.PlacePayload, str]:
+    data: schemas.user_io.UserInput,
+    )-> schemas.user_io.UserOutput:
     """Endpoint for receiving user input from frontend."""
 
     embedding: list[float] = services.ml.embedding_model(data.prompt)
     best_route: list[models.place_payload.PlacePayload] | None = (
         get_best_route(embedding, data.time_for_walk)
         )
-    output_dict: dict = {}
+    print(best_route)
+    print(len(best_route))
     
     if best_route is not None:
-        for place in best_route:
-            explanation = services.ml.text_generation_model.get_desc_selection(place)
-            output_dict[place.model_dump(exclude={'score'})] = explanation
+        explanation = services.ml.text_generation_model.get_desc_selection(
+            data.prompt, best_route,
+            )
+        print(explanation)
         
-        return output_dict
+        return {
+            'walking_time': data.time_for_walk + 1,
+            'walking_path': best_route,
+            'explanation': explanation,
+        }
                 
     
     return ValueError(
@@ -53,7 +59,8 @@ def get_best_route(
         shift: int = 0
         best_score: int = 0
         best_permutation: list[models.place_payload.PlacePayload] = []
-        while shift <= MAX_SHIFT or len_perm - shift >= 0:
+        while shift <= MAX_SHIFT and len_perm - shift >= 0:
+            print(shift)
             time_to_pass: int = len_perm - shift
             permutations: itertools.permutations[models.place_payload.PlacePayload] = (
                 itertools.permutations(places, len_perm - shift)
@@ -62,10 +69,11 @@ def get_best_route(
             for perm in permutations:
                 if time_to_pass - 1 > time_for_walk:
                     continue
-                perm_score = sum(perm, key=lambda x: x.score)
+                perm_score = sum(item.score for item in perm)
                 if perm_score > best_score:
                     best_permutation = perm[:]
                     best_score = perm_score
+            shift += 1
         
         if not best_permutation:
             permutations: itertools.permutations[models.place_payload.PlacePayload] = (
@@ -74,7 +82,7 @@ def get_best_route(
             for perm in permutations:
                     if time_to_pass - 1 > time_for_walk:
                         continue
-                    perm_score = sum(perm, key=lambda x: x.score)
+                    perm_score = sum(item.score for item in perm)
                     if perm_score > best_score:
                         best_permutation = perm[:]
                         best_score = perm_score
